30f4ca4d820804a4fe4d0f959cbbfd8d7bbd78f5
andrewpolar <61638749+andrewpolar@users.noreply.github.com>
2026-02-25 16:54:40 -0500

    Update DD33C.cpp

--- DD33C.cpp
+++ DD33C.cpp
@@ -261,4 +261,310 @@ int main() {
 	printf("Averate error %d, target ranges [%d, %d]\n", sum, actualTargetMin, actualTargetMax);
 }
 
+//RTL code for FPGA 
+//module DD33 #(
+//    //data
+//    parameter shortint N_FEATURES = 9,
+//    parameter shortint N_XMIN = 0,
+//    parameter shortint N_TARGETMIN = -20480,
+//    parameter shortint N_TARGETMAX = 20480,
+//
+//    //network inner
+//    parameter shortint N_INNER = 16,
+//    parameter shortint N_INNER_POINTS = 3,
+//    parameter shortint N_INNER_SHIFT = 7,
+//    parameter shortint N_INNER_LEARNING_SHIFT = 4,
+//    parameter shortint MULT_INNER = 113,
+//    parameter shortint DENOMINNER = 10,
+//
+//    //network outer
+//    parameter shortint N_OUTER = 1,
+//    parameter shortint N_OUTER_POINTS = 21,
+//    parameter shortint N_OUTER_SHIFT = 11,
+//    parameter shortint N_OUTER_LEARNING_SHIFT = 2,
+//    parameter shortint MULT_OUTER = 1,
+//    parameter shortint DENOMOUTER = 4,
+//
+//    //training
+//    parameter int N_PROCESSED_RECORDS = 50000
+//    )(
+//        input  logic        CLK100MHZ,
+//        input  logic        CPU_RESETN,
+//        output logic[15:0] LED
+//        );
+//
+//logic signed[31:0] intermediate_matrix[0:N_INNER - 1][0:N_FEATURES - 1];
+//logic signed[31:0] intermediate_vector[0:N_INNER - 1];
+//logic signed[31:0] INNER[0:N_INNER * N_FEATURES - 1][0:N_INNER_POINTS - 1];
+//logic signed[31:0] innerOffset[0:N_INNER * N_FEATURES - 1];
+//logic signed[31:0] innerIndex[0:N_INNER * N_FEATURES - 1];
+//
+//logic signed[31:0] outer_vector[0:N_INNER - 1];
+//logic signed[31:0] OUTER[0:N_INNER - 1][0:N_OUTER_POINTS - 1];
+//logic signed[31:0] outerOffset[0:N_INNER - 1];
+//logic signed[31:0] outerIndex[0:N_INNER - 1];
+//logic signed[31:0] m;
+//logic[7:0] features[0:N_FEATURES - 1];
+//logic signed[31:0] predicted;
+//logic signed[31:0] outer_residual;
+//logic signed[31:0] error_buffer[0:255];
+//logic signed[31:0] inner_residual[0:N_INNER - 1];
+//logic[7:0] err_wr_ptr;
+//
+//logic signed[31:0] delta_F1_matrix[0:N_INNER - 1][0:N_FEATURES - 1];
+//logic signed[31:0] delta_F2_matrix[0:N_INNER - 1][0:N_FEATURES - 1];
+//logic signed[31:0] delta_F1[0:N_INNER - 1];
+//logic signed[31:0] delta_F2[0:N_INNER - 1];
+//
+//logic[15:0] seed[9] = '{
+//16'hACE1, 16'hBEEF, 16'h1234,
+//16'h4321, 16'hA777, 16'hAAAA,
+//16'h1F2F, 16'hCAFE, 16'hBABE
+//    };
+//    logic[15:0] seed16 = 16'h0DA0;  
+//        logic[7:0] rnd_bytes[9];
+//    int cnt, range, mean, left, right;
+//    logic signed[31:0] actualTargetMin;
+//    logic signed[31:0] actualTargetMax;
+//    logic signed[31:0] target;
+//    logic signed[31:0] sum;
+//
+//    initial begin
+//
+//        //initialization of models
+//        range = (N_TARGETMAX - N_TARGETMIN) >> 2;
+//    mean = (N_TARGETMAX + N_TARGETMIN) >> 1;
+//    left = mean - range;
+//    right = mean + range;
+//
+//    for (int i = 0; i < N_INNER * N_FEATURES; i++) begin
+//        for (int j = 0; j < N_INNER_POINTS; j++) begin
+//            INNER[i][j] = rnd_range_fast(seed16, left, right);
+//    end
+//        end
+//
+//        for (int i = 0; i < N_INNER; i++) begin
+//            for (int j = 0; j < N_OUTER_POINTS; j++) begin
+//                OUTER[i][j] = rnd_range_fast(seed16, left, right);
+//    end
+//        end
+//
+//        cnt = 0;
+//    actualTargetMin = -1;
+//    actualTargetMax = 1;
+//    err_wr_ptr = 0;
+//    while (1) begin
+//        //making new record
+//        rnd9(features);
+//    target = det3x3(features);
+//
+//    if (target < actualTargetMin) actualTargetMin = target;
+//    if (target > actualTargetMax) actualTargetMax = target;
+//
+//    //forward pass
+//    for (int k = 0; k < N_INNER; ++k) begin
+//        for (int j = 0; j < N_FEATURES; ++j) begin
+//            m = k * N_FEATURES + j;
+//    intermediate_matrix[k][j] = ComputeInner(features[j] - N_XMIN, N_INNER_SHIFT, INNER[m], innerOffset[m], innerIndex[m]);
+//    end
+//        end
+//
+//        for (int k = 0; k < N_INNER; ++k) begin
+//            intermediate_vector[k] = ReduceInner(intermediate_matrix[k], MULT_INNER, DENOMINNER, N_TARGETMIN, N_TARGETMAX);
+//    end
+//
+//        for (int j = 0; j < N_INNER; ++j) begin
+//            outer_vector[j] = ComputeOuter(intermediate_vector[j] - N_TARGETMIN, N_OUTER_SHIFT, OUTER[j], outerOffset[j], outerIndex[j]);
+//    end
+//
+//        predicted = ReduceOuter(outer_vector, MULT_OUTER, DENOMOUTER, N_TARGETMIN, N_TARGETMAX);
+//    //end of forward pass
+//
+//    //start of the back propagation
+//    outer_residual = target - predicted;
+//    error_buffer[err_wr_ptr] = target - predicted;
+//    err_wr_ptr = err_wr_ptr + 1;
+//
+//    for (int j = 0; j < N_INNER; ++j) begin
+//        inner_residual[j] = GetInnerResidual(outer_residual, N_OUTER_SHIFT, OUTER[j], outerIndex[j]);
+//    end
+//
+//        for (int j = 0; j < N_INNER; ++j) begin
+//            delta_F1[j] = ((outer_residual >> > N_OUTER_LEARNING_SHIFT) * outerOffset[j]) >> > N_OUTER_SHIFT;
+//    end
+//
+//        for (int j = 0; j < N_INNER; ++j) begin
+//            delta_F2[j] = (outer_residual >> > N_OUTER_LEARNING_SHIFT) - delta_F1[j];
+//    end
+//
+//        for (int k = 0; k < N_INNER; ++k) begin
+//            for (int j = 0; j < N_FEATURES; ++j) begin
+//                m = k * N_FEATURES + j;
+//    delta_F1_matrix[k][j] = ((inner_residual[k] >> > N_INNER_LEARNING_SHIFT) * innerOffset[m]) >> > N_INNER_SHIFT;
+//    end
+//        end
+//
+//        for (int k = 0; k < N_INNER; ++k) begin
+//            for (int j = 0; j < N_FEATURES; ++j) begin
+//                delta_F2_matrix[k][j] = (inner_residual[k] >> > N_INNER_LEARNING_SHIFT) - delta_F1_matrix[k][j];
+//    end
+//        end
+//        //end of back propagation step
+//
+//        //next is update step
+//        for (int j = 0; j < N_INNER; ++j) begin
+//            OUTER[j][outerIndex[j] + 1] += delta_F1[j];
+//    end
+//
+//        for (int j = 0; j < N_INNER; ++j) begin
+//            OUTER[j][outerIndex[j]] += delta_F2[j];
+//    end
+//
+//        for (int k = 0; k < N_INNER; ++k) begin
+//            for (int j = 0; j < N_FEATURES; ++j) begin
+//                m = k * N_FEATURES + j;
+//    INNER[m][innerIndex[m] + 1] += delta_F1_matrix[k][j];
+//    end
+//        end
+//
+//        for (int k = 0; k < N_INNER; ++k) begin
+//            for (int j = 0; j < N_FEATURES; ++j) begin
+//                m = k * N_FEATURES + j;
+//    INNER[m][innerIndex[m]] += delta_F2_matrix[k][j];
+//    end
+//        end
+//        //end update and record processing step
+//
+//        if (++cnt >= N_PROCESSED_RECORDS) break;
+//    if ((cnt & 16'h3FFF) == 0) begin
+//        $display("Training record: %0d", cnt);
+//    end
+//        end //end while loop
+//
+//        sum = 0;
+//    for (int i = 0; i < 256; i++) begin
+//        if (error_buffer[i] < 0) sum -= error_buffer[i];
+//        else sum += error_buffer[i];
+//    end
+//        sum = sum >> > 8;
+//    $display("Average error %0d, target ranges [%0d, %0d]", sum, actualTargetMin, actualTargetMax);
+//
+//    end //initial
+//
+//    // EXACT equivalent of:
+//    // static inline uint16_t lfsr16(uint16_t* state)
+//        function automatic logic[15:0] lfsr16_next(ref logic[15:0] state);
+//    logic bitval;
+//    bitval = (state >> 15) ^ (state >> 13) ^ (state >> 12) ^ (state >> 10);
+//    state = (state << 1) | (bitval & 1'b1);
+//        if (state == 16'h0000) state = 16'h0001;
+//            return state;
+//            endfunction
+//
+//            // EXACT equivalent of:
+//            // static inline uint8_t rnd_byte(uint16_t* state)
+//            function automatic logic[7:0] rnd_byte(ref logic[15:0] state);
+//            logic[15:0] s;
+//            logic[7:0]  v;
+//            s = lfsr16_next(state);
+//            v = s[7:0];
+//            if (v == 8'd0) v = 8'd1;
+//                if (v == 8'd255) v = 8'd254;
+//                    return v;
+//                    endfunction
+//
+//                    // Generate 9 bytes (same behavior as calling C 9 times)
+//                    function automatic void rnd9(output logic[7:0] data[9]);
+//                    for (int i = 0; i < 9; i++) begin
+//                        data[i] = rnd_byte(seed[i]);
+//                    end
+//                        endfunction
+//
+//                        function automatic logic signed[15:0] det3x3(input logic[7:0] m[0:8]);
+//    logic signed[31:0] a, b, c, d, e, f, g, h, i;
+//    logic signed[31:0] det;
+//    begin
+//        a = m[0]; b = m[1]; c = m[2];
+//    d = m[3]; e = m[4]; f = m[5];
+//    g = m[6]; h = m[7]; i = m[8];
+//    det = a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g);
+//    det = det >> > 10;
+//    det3x3 = det[15:0];
+//    end
+//        endfunction
+//
+//        function automatic int ComputeInner(input int diff, input  int delta_shift,
+//            input logic signed[31:0] row[0:N_INNER_POINTS - 1], output int offset, output int index);
+//    longint Q;
+//    index = diff >> > delta_shift;
+//    offset = diff & ((1 << delta_shift) - 1);
+//    Q = row[index + 1] - row[index];
+//    Q *= offset;
+//    Q >>>= delta_shift;
+//    Q += row[index];
+//    return int'(Q);
+//        endfunction
+//
+//        function automatic int ComputeOuter(input int diff, input  int delta_shift,
+//            input logic signed[31:0] row[0:N_OUTER_POINTS - 1], output int offset, output int index);
+//    longint Q;
+//    index = diff >> > delta_shift;
+//    offset = diff & ((1 << delta_shift) - 1);
+//    Q = row[index + 1] - row[index];
+//    Q *= offset;
+//    Q >>>= delta_shift;
+//    Q += row[index];
+//    return int'(Q);
+//        endfunction
+//
+//        function automatic int rnd_range_fast(ref logic[15:0] state, input int left, input int right);
+//    logic[31:0] span;
+//    logic[31:0] r;
+//    logic[31:0] scaled;
+//
+//    span = right - left + 1;
+//    r = lfsr16_next(state);
+//    scaled = (r * span) >> 16;
+//    return left + int'(scaled);
+//        endfunction
+//
+//        function automatic int ReduceInner(input logic signed[31:0] V[0:N_FEATURES - 1],
+//            input int mult, input int denom, input int min_val, input int max_val);
+//    longint x;
+//    x = 0;
+//    for (int i = 0; i < N_FEATURES; i++) begin
+//        x += V[i];
+//    end
+//        x *= mult;
+//    x >>>= denom;
+//    if (x <= min_val) x = min_val + 1;
+//    if (x >= max_val) x = max_val - 1;
+//    return int'(x);
+//        endfunction
+//
+//        function automatic int ReduceOuter(input logic signed[31:0] V[0:N_INNER - 1],
+//            input int mult, input int denom, input int min_val, input int max_val);
+//    longint x;
+//    x = 0;
+//    for (int i = 0; i < N_INNER; i++) begin
+//        x += V[i];
+//    end
+//        x *= mult;
+//    x >>>= denom;
+//    if (x <= min_val) x = min_val + 1;
+//    if (x >= max_val) x = max_val - 1;
+//    return int'(x);
+//        endfunction
+//
+//        function automatic int GetInnerResidual(input int outer_residual, input int outer_shift,
+//            input logic signed[31:0] row[0:N_OUTER_POINTS - 1], input int index);
+//    longint product;
+//    begin
+//        product = (row[index + 1] - row[index]) * outer_residual;
+//    GetInnerResidual = int'(product >>> outer_shift); 
+//        end
+//        endfunction
+//
+//        endmodule
+
 
